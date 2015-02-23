ff0e89d728113cde3fc43b7384a827110be92e3f
kavi87 <kaviramyead@gmail.com>
2015-02-23 13:27:49 +0000

    added grunt task for transpiling less

---
+++ src/gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    'main/js/css/w20-business-theme.css': 'less/main.less'
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['less/**/*.less'],
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['less', 'watch']);
+};
