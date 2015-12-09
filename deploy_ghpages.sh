55cb6037598a796a0602790fcfccf8a87144256d
kavi87 <kaviramyead@gmail.com>
2015-12-09 20:23:40 +0100

    fixed output issue in travis

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -23,5 +23,5 @@
 	git add .
 	git add bower_components -f
 	git commit -m "Built for gh-pages of http://seedstack.github.io/w20-business-theme"
-	git push --force "https://${GITHUB_TOKEN}@github.com/seedstack/w20-business-theme" master:gh-pages
+	git push --force --quiet "https://${GITHUB_TOKEN}@github.com/seedstack/w20-business-theme" master:gh-pages >/dev/null 2>&1
 )
